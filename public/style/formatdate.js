906d8e454c00a5827aab75ba2f9b5b1e10c2624d
indrariksa <indrariksa@gmail.com>
2023-11-14 14:02:00 +0700

    feat(formatTanggalWaktu): membuat fungsi format waktu dan tanggal

--- public/style/formatdate.js
+++ public/style/formatdate.js
@@ -2,3 +2,21 @@
 export function formatTwoDigits(number) {
     return (number < 10 ? '0' : '') + number;
 }
+
+export function formatTanggalWaktu(date) {
+    const options = {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    };
+    const formattedDate = new Date(date).toLocaleDateString('id-ID', options);
+
+    const timeOptions = {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    };
+    const formattedTime = new Date(date).toLocaleTimeString(timeOptions);
+
+    return `${formattedDate} || ${formattedTime}`;
+}
