20e1bc9e323e20a68c873f926c418dec09cb1851
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-14 12:12:28 +0700

    feat(pagination) : add pagination untuk get pendaftar

--- public/js_inti/pembuat_akun.js
+++ public/js_inti/pembuat_akun.js
@@ -1,22 +1,31 @@
 // Import library yang dibutuhkan
 import { UrlGetPendaftar } from "../controller/template.js";
 import { formatTwoDigits } from "../style/formatdate.js"
+import { CihuyDomReady, CihuyQuerySelector } from "https://c-craftjs.github.io/table/table.js";
+import { CihuyId } from "https://c-craftjs.github.io/element/element.js";
 
-// Untuk Get All Data Pendaftar
-fetch(UrlGetPendaftar)
-	.then((result) => {
-		return result.json();
-	})
-	.then((data) => {
-		let tableData = "";
-		data.data.map((values, index) => {
+CihuyDomReady(() => {
+    const tablebody = CihuyId("tablebody");
+    const buttonPreviousPage = CihuyId("prevPageBtn");
+    const buttonNextPage = CihuyId("nextPageBtn");
+    const halamanSaatIni = CihuyId("currentPage");
+    const itemPerPage = 10;
+    let halamannow = 1;
 
+    // Untuk Get All Data Pendaftar
+    fetch(UrlGetPendaftar)
+    .then((result) => {
+        return result.json();
+    })
+    .then((data) => {
+        let tableData = "";
+        data.data.map((values, index) => {
         // Ubah format tanggal
         const tglDaftar = new Date(values.tgl_daftar_mhs);
         const formattedTglDaftar = `${formatTwoDigits(tglDaftar.getDate())}-${formatTwoDigits(tglDaftar.getMonth() + 1)}-${tglDaftar.getFullYear()} (${tglDaftar.toLocaleTimeString()})`;
 
-			// Manipulasi data pegawai dan masukkan ke dalam bentuk tabel
-			tableData += `
+            // Manipulasi data pegawai dan masukkan ke dalam bentuk tabel
+            tableData += `
                         <tr>
                         <td>
                         <div class="min-width">
@@ -47,10 +56,57 @@ fetch(UrlGetPendaftar)
                             <a href="#" class="btn btn-danger" role="button">Hapus</a>
                         </td>
                     </tr>`;
-		});
-		// Tampilkan data pegawai ke dalam tabel
-		document.getElementById("tablebody").innerHTML = tableData;
-	})
-	.catch(error => {
-		console.log('error', error);
+        });
+        // Tampilkan data pegawai ke dalam tabel
+        document.getElementById("tablebody").innerHTML = tableData;
+
+        // Untuk Memunculkan Pagination Halamannya
+        displayData(halamannow);
+        updatePagination();
+    })
+    .catch(error => {
+        console.log('error', error);
+    });
+
+    // Fungsi untuk Menampilkan Data
+	function displayData(page) {
+		const baris = CihuyQuerySelector("#tablebody tr");
+		const mulaiindex = (page - 1) * itemPerPage;
+		const akhirindex = mulaiindex + itemPerPage;
+
+		for (let i = 0; i < baris.length; i++) {
+			if (i >= mulaiindex && i < akhirindex) {
+				baris[i].style.display = "table-row";
+			} else {
+				baris[i].style.display = "none";
+			}
+		}
+	}
+
+    // Fungsi untuk Update Pagination
+    function updatePagination() {
+        halamanSaatIni.textContent = `Halaman ${halamannow}`;
+    }
+
+    // Button Pagination (Sebelumnya)
+    buttonPreviousPage.addEventListener("click", () => {
+        if (halamannow > 1) {
+            halamannow--;
+            displayData(halamannow);
+            updatePagination();
+        }
+    });
+
+    // Button Pagination (Selanjutnya)
+	buttonNextPage.addEventListener("click", () => {
+		const totalPages = Math.ceil(
+			tablebody.querySelectorAll("#tablebody tr").length / itemPerPage
+		);
+		if (halamannow < totalPages) {
+			halamannow++;
+			displayData(halamannow);
+			updatePagination();
+		}
 	});
+});
+
