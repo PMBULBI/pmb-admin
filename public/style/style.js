da79a81fc0dd1264547e72a4190404d5458cb26b
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-15 09:37:58 +0700

    add(spinner) : add style spinner with js

--- public/style/style.js
+++ public/style/style.js
@@ -21,3 +21,14 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+
+// Spinner Loader
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+
+  loader.classList.add("loader-hidden");
+
+  loader.addEventListener("transitioned", () => {
+      document.body.removeChild("loader");
+  })
+})
