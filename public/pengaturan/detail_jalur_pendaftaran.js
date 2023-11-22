a42a7a6b46092e533630de808477ec6b7124cf94
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-22 14:39:58 +0700

    feat(post) : coba post jalur pendaftaran

--- public/pengaturan/detail_jalur_pendaftaran.js
+++ public/pengaturan/detail_jalur_pendaftaran.js
@@ -1,4 +1,5 @@
-import { UrlGetJalurById, UrlPutJalur } from "../controller/template.js";
+import { UrlGetJalurById, UrlPostJalur, UrlPutJalur } from "../controller/template.js";
+import { token } from "../controller/cookies.js";
 
 // Get Data Jalur Pendaftaran By Id
 // Ambil terlebih dahulu id dari URL
@@ -22,6 +23,87 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         })
         .catch(error => console.error('Terjadi kesalahan:', error));
+});
+
+// Post Data Jalur Pendaftaran
+var header = new Headers();
+header.append("login", token);
+header.append("Content-Type", "application/json");
+
+function addJalurPendaftaran(postData) {
+    fetch(UrlPostJalur, {
+        method: 'POST',
+        headers: header,
+        body: JSON.stringify(postData)
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.success) {
+        // Display success SweetAlert
+        Swal.fire({
+          icon: 'success',
+          title: 'Jalur Pendaftaran Berhasil Ditambahkan!',
+        }).then(() => {
+          // Refresh the page after successful addition
+          window.location.href = 'seluruh-karyawan.html';
+        });
+      } else {
+        // Display error SweetAlert
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Jalur Pendaftaran Gagal Ditambahkan!',
+        });
+      }
+    })
+    .catch(error => {
+      console.error("Error while adding employee data:", error);
+    });
+}
+
+// Event listener for the "Tambah Karyawan" button
+const tambahButton = document.querySelector('#tambahButton');
+tambahButton.addEventListener('click', () => {
+  // Get input values
+  const jalurInput = document.querySelector('#jalur').value;
+  const namaJalurInput = document.querySelector('#nama_jalur').value;
+  const keteranganJalurInput = document.querySelector('#keterangan_jalur').value;
+  const statusInput = document.querySelector('#status').value;
+  
+  // Check if any of the fields is empty
+  if (!jalurInput || !namaJalurInput || !keteranganJalurInput || !statusInput) {
+    Swal.fire({
+      icon: 'warning',
+      title: 'Oops...',
+      text: 'Semua field harus diisi!',
+    });
+    return; // Stop further processing
+  }
+
+  // Create a data object to be sent
+  const postData = {
+    jalur: jalurInput,
+    nama_jalur: namaJalurInput,
+    keterangan_jalur: keteranganJalurInput,
+    status: statusInput
+  };
+  
+  // Display SweetAlert for confirmation
+  Swal.fire({
+    title: 'Tambah Jalur',
+    text: 'Anda Yakin Menambah Jalur Pendaftaran?',
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes',
+    cancelButtonText: 'No'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // Call function to add employee data
+      addJalurPendaftaran(postData);
+    }
+  });
 });
 
 // Put Data Jalur Pendaftaran By Id
