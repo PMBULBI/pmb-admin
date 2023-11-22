e4f08c69c7722a7e4aca82813d611cd8b3e5d7b1
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-22 11:14:35 +0700

    feat(getjalur) : get jalur pendaftaran

---
+++ public/pengaturan/jalur_pendaftaran.js
@@ -0,0 +1,110 @@
+// Import library yang dibutuhkan
+import { UrlGetJalur } from "../controller/template.js";
+import { CihuyDomReady, CihuyQuerySelector } from "https://c-craftjs.github.io/table/table.js";
+import { CihuyId } from "https://c-craftjs.github.io/element/element.js";
+
+CihuyDomReady(() => {
+    const tablebody = CihuyId("tablebody");
+    const buttonPreviousPage = CihuyId("prevPageBtn");
+    const buttonNextPage = CihuyId("nextPageBtn");
+    const halamanSaatIni = CihuyId("currentPage");
+    const itemPerPage = 10;
+    let halamannow = 1;
+
+    // Untuk Get All Data Pendaftar
+    fetch(UrlGetJalur)
+    .then((result) => {
+        return result.json();
+    })
+    .then((data) => {
+        let tableData = "";
+        data.data.map((values, index) => {
+            // Manipulasi data pegawai dan masukkan ke dalam bentuk tabel
+            tableData += `
+                        <tr style="text-align: center; vertical-align: middle">
+                        <td>
+                        <div class="min-width">
+                            <p>${index + 1}</p>
+                        </div>
+                        </td>
+                        <td class="min-width">
+                            <p>${values.jalur}</p>
+                        </td>
+                        <td class="min-width">
+                            <p>${values.nama_jalur}</p>
+                        </td>
+                        <td class="min-width">
+                            <p>${values.keterangan_jalur}</p>
+                        </td>
+                        <td class="min-width">
+                            <p>${values.status}</p>
+
+                        </td>
+                        <td style="text-align: center; vertical-align: middle">
+                            <button type="button" class="btn btn-info" style="color: white;" data-pembuat-akun=${values.id} >Edit</button>
+                            <button type="button" class="btn btn-danger" data-pembuat-akun=${values.id}>Hapus</button>
+                        </td>
+                    </tr>`;
+        });
+        // Tampilkan data pegawai ke dalam tabel
+        document.getElementById("tablebody").innerHTML = tableData;
+
+        // Untuk Button Detail
+        const detailButton = document.querySelectorAll(".btn-warning");
+        detailButton.forEach(button => {
+            button.addEventListener('click', (event) => {
+                const id = event.target.getAttribute('data-pembuat-akun');
+                window.location.href = `inti/detail-pembuat-akun.html?id=${id}`;
+            });
+        });
+
+        // Untuk Memunculkan Pagination Halamannya
+        displayData(halamannow);
+        updatePagination();
+    })
+    .catch(error => {
+        console.log('error', error);
+    });
+
+    // Fungsi untuk Menampilkan Data
+	function displayData(page) {
+		const baris = CihuyQuerySelector("#tablebody tr");
+		const mulaiindex = (page - 1) * itemPerPage;
+		const akhirindex = mulaiindex + itemPerPage;
+
+		for (let i = 0; i < baris.length; i++) {
+			if (i >= mulaiindex && i < akhirindex) {
+				baris[i].style.display = "table-row";
+			} else {
+				baris[i].style.display = "none";
+			}
+		}
+	}
+
+    // Fungsi untuk Update Pagination
+    function updatePagination() {
+        halamanSaatIni.textContent = `Halaman ${halamannow}`;
+    }
+
+    // Button Pagination (Sebelumnya)
+    buttonPreviousPage.addEventListener("click", () => {
+        if (halamannow > 1) {
+            halamannow--;
+            displayData(halamannow);
+            updatePagination();
+        }
+    });
+
+    // Button Pagination (Selanjutnya)
+	buttonNextPage.addEventListener("click", () => {
+		const totalPages = Math.ceil(
+			tablebody.querySelectorAll("#tablebody tr").length / itemPerPage
+		);
+		if (halamannow < totalPages) {
+			halamannow++;
+			displayData(halamannow);
+			updatePagination();
+		}
+	});
+});
+
