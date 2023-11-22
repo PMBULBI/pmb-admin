9519da18d2818da63d4e51f829b15647b8e099ea
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-22 11:00:48 +0700

    add(endpointjalur) : get post put delete

--- public/controller/template.js
+++ public/controller/template.js
@@ -2,20 +2,25 @@
 export let UrlGetPendaftar = "https://komarbe.ulbi.ac.id/pendaftar/pendaftar";
 export let UrlGetAdminLevel = "https://komarbe.ulbi.ac.id/admin_level";
 export let UrlGetAdmin = "https://komarbe.ulbi.ac.id/admin";
+export let UrlGetJalur = "https://komarbe.ulbi.ac.id/jalur";
 
 // Endpoint GET by Id
 export let UrlGetPendaftarById = "https://komarbe.ulbi.ac.id/pendaftar/pendaftar/get";
 export let UrlGetAdminLevelById = "https://komarbe.ulbi.ac.id/admin_level/get/";
 export let UrlGetAdminById = "https://komarbe.ulbi.ac.id/admin/get/";
+export let UrlGetJalurById = "https://komarbe.ulbi.ac.id/jalur/get/";
 
 // Endpoint POST
 export let UrlPostAdminLevel = "https://komarbe.ulbi.ac.id/admin_level/post/";
 export let UrlPostAdmin = "https://komarbe.ulbi.ac.id/admin/post/";
+export let UrlPostJalur = "https://komarbe.ulbi.ac.id/jalur/post/";
 
 // Endpoint PUT
 export let UrlPutAdminLevel = "https://komarbe.ulbi.ac.id/admin_level/put/";
 export let UrlPutAdmin = "https://komarbe.ulbi.ac.id/admin/put/";
+export let UrlPutJalur = "https://komarbe.ulbi.ac.id/jalur/post/";
 
 // Endpoint DELETE
 export let UrlDeleteAdminLevel = "https://komarbe.ulbi.ac.id/admin_level/delete/";
 export let UrlDeleteAdmin = "https://komarbe.ulbi.ac.id/admin/delete/";
+export let UrlDeleteJalur = "https://komarbe.ulbi.ac.id/jalur/post/";
