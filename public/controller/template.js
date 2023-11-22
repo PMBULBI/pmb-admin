3025fc2db8bacc8a2e703431904db069ccd23b50
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-22 10:57:58 +0700

    add(endpointadmin) : get post put delete

--- public/controller/template.js
+++ public/controller/template.js
@@ -1,16 +1,21 @@
 // Endpoint GET
 export let UrlGetPendaftar = "https://komarbe.ulbi.ac.id/pendaftar/pendaftar";
 export let UrlGetAdminLevel = "https://komarbe.ulbi.ac.id/admin_level";
+export let UrlGetAdmin = "https://komarbe.ulbi.ac.id/admin";
 
 // Endpoint GET by Id
 export let UrlGetPendaftarById = "https://komarbe.ulbi.ac.id/pendaftar/pendaftar/get";
 export let UrlGetAdminLevelById = "https://komarbe.ulbi.ac.id/admin_level/get/";
+export let UrlGetAdminById = "https://komarbe.ulbi.ac.id/admin/get/";
 
 // Endpoint POST
 export let UrlPostAdminLevel = "https://komarbe.ulbi.ac.id/admin_level/post/";
+export let UrlPostAdmin = "https://komarbe.ulbi.ac.id/admin/post/";
 
 // Endpoint PUT
 export let UrlPutAdminLevel = "https://komarbe.ulbi.ac.id/admin_level/put/";
+export let UrlPutAdmin = "https://komarbe.ulbi.ac.id/admin/put/";
 
 // Endpoint DELETE
-export let UrlDeleteAdminLevel = "https://komarbe.ulbi.ac.id/admin_level/delete/"
+export let UrlDeleteAdminLevel = "https://komarbe.ulbi.ac.id/admin_level/delete/";
+export let UrlDeleteAdmin = "https://komarbe.ulbi.ac.id/admin/delete/";
