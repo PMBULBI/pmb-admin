5d52f86cb5f9a39118e9d761cc084521b27ebeff
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-22 15:15:20 +0700

    feat(getdata) : get all data fakultas

---
+++ public/pengaturan/fakultas.js
@@ -0,0 +1,100 @@
+// Import library yang dibutuhkan
+import { UrlGetFakultas } from "../controller/template.js";
+import { CihuyDomReady, CihuyQuerySelector } from "https://c-craftjs.github.io/table/table.js";
+import { CihuyId } from "https://c-craftjs.github.io/element/element.js";
+
+// Get Data Jalur Pendaftaran
+CihuyDomReady(() => {
+    const tablebody = CihuyId("tablebody");
+    const buttonPreviousPage = CihuyId("prevPageBtn");
+    const buttonNextPage = CihuyId("nextPageBtn");
+    const halamanSaatIni = CihuyId("currentPage");
+    const itemPerPage = 10;
+    let halamannow = 1;
+
+    // Untuk Get All Data Pendaftar
+    fetch(UrlGetFakultas)
+    .then((result) => {
+        return result.json();
+    })
+    .then((data) => {
+        let tableData = "";
+        data.data.map((values, index) => {
+            // Manipulasi data pegawai dan masukkan ke dalam bentuk tabel
+            tableData += `
+                        <tr style="text-align: center; vertical-align: middle">
+                        <td>
+                        <div class="min-width">
+                            <p>${index + 1}</p>
+                        </div>
+                        </td>
+                        <td class="min-width">
+                            <p>${values.nama_fakultas}</p>
+                        </td>
+                        <td style="text-align: center; vertical-align: middle">
+                            <button type="button" class="btn btn-warning" style="color: white;" fakultas=${values.id_jalur} >Edit</button>
+                            <button type="button" class="btn btn-danger" fakultas=${values.id_jalur}>Hapus</button>
+                        </td>
+                    </tr>`;
+        });
+        // Tampilkan data pegawai ke dalam tabel
+        document.getElementById("tablebody").innerHTML = tableData;
+
+        // // Untuk Button Detail
+        // const detailButton = document.querySelectorAll(".btn-warning");
+        // detailButton.forEach(button => {
+        //     button.addEventListener('click', (event) => {
+        //         const id_jalur = event.target.getAttribute('fakultas');
+        //         window.location.href = `detail_jalur_pendaftaran.html?id=${id_jalur}`;
+        //     });
+        // });
+
+        // Untuk Memunculkan Pagination Halamannya
+        displayData(halamannow);
+        updatePagination();
+    })
+    .catch(error => {
+        console.log('error', error);
+    });
+
+    // Fungsi untuk Menampilkan Data
+	function displayData(page) {
+		const baris = CihuyQuerySelector("#tablebody tr");
+		const mulaiindex = (page - 1) * itemPerPage;
+		const akhirindex = mulaiindex + itemPerPage;
+
+		for (let i = 0; i < baris.length; i++) {
+			if (i >= mulaiindex && i < akhirindex) {
+				baris[i].style.display = "table-row";
+			} else {
+				baris[i].style.display = "none";
+			}
+		}
+	}
+
+    // Fungsi untuk Update Pagination
+    function updatePagination() {
+        halamanSaatIni.textContent = `Halaman ${halamannow}`;
+    }
+
+    // Button Pagination (Sebelumnya)
+    buttonPreviousPage.addEventListener("click", () => {
+        if (halamannow > 1) {
+            halamannow--;
+            displayData(halamannow);
+            updatePagination();
+        }
+    });
+
+    // Button Pagination (Selanjutnya)
+	buttonNextPage.addEventListener("click", () => {
+		const totalPages = Math.ceil(
+			tablebody.querySelectorAll("#tablebody tr").length / itemPerPage
+		);
+		if (halamannow < totalPages) {
+			halamannow++;
+			displayData(halamannow);
+			updatePagination();
+		}
+	});
+});
