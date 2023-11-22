93b942001b123b9b8adfe76feb531923ff316ede
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-22 10:56:08 +0700

    add(endpointadminlevel) : get post put delete endpoint

--- public/controller/template.js
+++ public/controller/template.js
@@ -1,3 +1,16 @@
 // Endpoint GET
 export let UrlGetPendaftar = "https://komarbe.ulbi.ac.id/pendaftar/pendaftar";
+export let UrlGetAdminLevel = "https://komarbe.ulbi.ac.id/admin_level";
+
+// Endpoint GET by Id
 export let UrlGetPendaftarById = "https://komarbe.ulbi.ac.id/pendaftar/pendaftar/get";
+export let UrlGetAdminLevelById = "https://komarbe.ulbi.ac.id/admin_level/get/";
+
+// Endpoint POST
+export let UrlPostAdminLevel = "https://komarbe.ulbi.ac.id/admin_level/post/";
+
+// Endpoint PUT
+export let UrlPutAdminLevel = "https://komarbe.ulbi.ac.id/admin_level/put/";
+
+// Endpoint DELETE
+export let UrlDeleteAdminLevel = "https://komarbe.ulbi.ac.id/admin_level/delete/"
