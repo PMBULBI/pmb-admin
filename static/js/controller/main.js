859ca7fac5c34c81657a8bcef181596cbf06f60a
JPratama7 <josepratama080@gmail.com>
2023-12-14 18:13:53 +0700

    fix: replace

---
+++ static/js/controller/main.js
@@ -0,0 +1,26 @@
+import { setInner } from "https://jscroot.github.io/element/croot.js";
+import { getWithHeader } from "https://jscroot.github.io/api/croot.js";
+// import { getCookie, setCookieWithExpireHour } from "https://jscroot.github.io/cookie/croot.js";
+// import { getHash } from "https://jscroot.github.io/url/croot.js";
+
+
+const main = async () => {
+
+    await getWithHeader("https://komarbe.ulbi.ac.id/admin/token", "LOGIN", token, dataAdmin);
+
+
+
+}
+
+
+const dataAdmin = (res) =>{
+    if (!res.success){
+        window.location.replace("../");
+    }
+    setInner("namaadmin", res.data.nama_admin);
+    setInner("hpadmin", res.data.no_hp);
+    setInner("emailadmin", res.data.email);
+}
+
+
+main();
